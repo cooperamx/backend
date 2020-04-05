f1ac2df0a85e3043b26190c8cac289df289837a2
sortigoza <saul.ortigoza@wizeline.com>
2020-04-05 16:03:37 -0700

    sortigoza/preparing-repo

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# use this script to build and push the docker image using gcloud build.
+# example usage:
+#   bash ./bin/build.sh
+
 set -eu
 
 : "${PROJECT_ID}"
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# use this script to deploy a docker image to cloud run.
+# example usage:
+#   bash ./bin/deploy.sh
+
 set -eu
 
 : "${PROJECT_ID}"
 : "${IMAGE}"
 
-gcloud run deploy --image "gcr.io/$PROJECT_ID/$IMAGE" --platform managed
+gcloud run deploy ${CLOUD_RUN_SERVICE} \
+    --image "gcr.io/$PROJECT_ID/$IMAGE" \
+    --platform managed \
+    --region ${GCP_REIGION}
