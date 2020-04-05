1db1c550a4ec21cf170b2f1171a677953734c5fc
sortigoza <saul.ortigoza@wizeline.com>
2020-04-05 16:57:15 -0700

    update message

--- src/app.js
+++ src/app.js
@@ -4,6 +4,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello Coopera.mx!'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
